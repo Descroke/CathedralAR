cbedfdaeb68406a59ffee8acb0332e5f6e14afe7
Unknown <jameskumru@gmail.com>
2018-04-17 16:49:30 +0100

    Added Sound, Text, Triggers, Second Object and Log File
    I've made a lot of progress here.
    
    - Changed markers (They have to be pretty different for each object, hence the uni logo and the cathedral logo.
    
    - Added Vase object along with target marker.
    
    - Added two sound files which are played each time the objects are loaded.
    
    - Two scripts made which are triggered each time an object is loaded. This script makes it so each time an object is tracked, it's sound is played (different for each one) and a simple "Object X tracked: True" string to a text log file which is saved locally on the device. (android/data/com.Group1CathedralAR/files/testFile.txt"

---
+++ Unity File/CathedralArGroup1/Assets/ImageTargetPlayAudio.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System.IO;
+
+
+public class ImageTargetPlayAudio : MonoBehaviour,
+ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void saveTrackedItem()
+    {
+        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "testFile.txt");
+
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePath, true))
+        {
+            file.WriteLine("\n Object 1 tracked: True");
+        }
+    }
+
+    public bool tracked = false;
+
+    public void OnTrackableStateChanged(
+    TrackableBehaviour.Status previousStatus,
+    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+        newStatus == TrackableBehaviour.Status.TRACKED ||
+        newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            GetComponent<AudioSource>().Play();
+
+            if(tracked == false)
+            {
+                saveTrackedItem();
+                tracked = true;
+            }
+
+        }
+        else
+        {
+            // Stop audio when target is lost
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
