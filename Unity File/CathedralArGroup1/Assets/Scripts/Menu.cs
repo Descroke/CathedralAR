1ba964aafe1e43ea0ef04c11a60b009abe34be20
Unknown <aidanj97@live.co.uk>
2018-04-24 21:12:08 +0100

    Added a UI
    Slightly reorganised with a few folders 
    Added a UI
    Scroll for 'What to find' never stops and can be lost
    need to have a camera in the background or graphical errors?

---
+++ Unity File/CathedralArGroup1/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject ItemImagePrefab;
+    public GameObject ItemImageContainer;
+
+    private void Start()
+    {
+        Sprite[] items = Resources.LoadAll<Sprite>("Items");
+
+        foreach (Sprite item in items)
+        {
+            GameObject container = Instantiate (ItemImagePrefab) as GameObject;
+            container.GetComponent<Image>().sprite = item;
+
+            container.transform.SetParent(ItemImageContainer.transform,false);
+
+
+        }
+    }
+
+}
