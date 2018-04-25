149a40a48b98ece2f8f392bc2c5a06c299230435
James <jameskumru@gmail.com>
2018-04-25 15:16:21 +0100

    Changed the WTF Menue to remove items already seen
    WTF Menu script checks the log file created and removes any already seen items from the list. Does this each time the menu is loaded.

--- Unity File/CathedralArGroup1/Assets/Scripts/Menu.cs
+++ Unity File/CathedralArGroup1/Assets/Scripts/Menu.cs
@@ -12,12 +12,50 @@ public class Menu : MonoBehaviour
     {
         Sprite[] items = Resources.LoadAll<Sprite>("Items");
 
+        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "testFile.txt");
+
+
+        bool object1 = false;
+        bool object2 = false;
+
+        try
+        {
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                if (line == " Object 1 tracked: True")
+                {
+                    object1 = true;
+                }
+                else if (line == " Object 2 tracked: True")
+                {
+                    object2 = true;
+
+                };
+            }
+        }
+        catch { };
+       
+
         foreach (Sprite item in items)
         {
-            GameObject container = Instantiate (ItemImagePrefab) as GameObject;
-            container.GetComponent<Image>().sprite = item;
+            if(item.name == "uniLincoln1" && object1 == true)
+            {
+                //nothing
+            }
+            else if(item.name == "Lincoln-Cathedral" && object2 == true)
+            {
+                //nothing
+            }
+            else
+            {
+                GameObject container = Instantiate(ItemImagePrefab) as GameObject;
+                container.GetComponent<Image>().sprite = item;
 
-            container.transform.SetParent(ItemImageContainer.transform,false);
+                container.transform.SetParent(ItemImageContainer.transform, false);
+            }
+
+
 
 
         }
